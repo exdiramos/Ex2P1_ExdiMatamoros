e1ec5033db7e2a854ed89d536308609e52aea98c
exdiramos <132028383+exdiramos@users.noreply.github.com>
2023-12-15 17:05:54 -0600

    Add files via upload
    Commit 4

--- src/ex2p1_exdimatamoros/Ex2P1_ExdiMatamoros.java
+++ src/ex2p1_exdimatamoros/Ex2P1_ExdiMatamoros.java
@@ -16,63 +16,61 @@ public class Ex2P1_ExdiMatamoros {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+   public static void main(String[] args) {
+        // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
-        int opcion = 0;
-        boolean condicion = true;
-        while (condicion){
-            System.out.println("-----MASS EFFECT-----");
-            System.out.println("1.Reclutar Tripulante ");
-            System.out.println("2.Listar Tripulacion ");
-            System.out.println("3.Batalla Final ");
-            System.out.println("4.Exit ");
-            System.out.println("---------------------");
-            System.out.print("Ingrese una opcion: ");
-            
+        ArrayList<Tripulante> tripulacion = new ArrayList<>();
+        int opcion;
+        do {
+            System.out.println("----------Mass Effect----------");
+            System.out.println("1. Reclutar tripulante");
+            System.out.println("2. Listar tripulacion");
+            System.out.println("3. Batalla final");
+            System.out.println("4.Salir");
+            System.out.println("-------------------------------");
+            System.out.print("Ingrese su opcion: ");
+
             opcion = entrada.nextInt();
-            
-            switch (opcion){
+            switch (opcion) {
                 case 1:
-                {
-                    ArrayList<Tripulante> tripulacion = null;
                     reclutarTripulante(tripulacion);
-                }
                     break;
-
                 case 2:
-                {
-                    ArrayList<Tripulante> tripulacion = null;
                     listarTripulantes(tripulacion);
-                }
                     break;
-
                 case 3:
-                {
-                    ArrayList<Tripulante> tripulacion = null;
-                    ataqueCollector(tripulacion);
-                }
+                    if (!tripulacion.isEmpty()) {
+                        if (batalla(tripulacion)) {
+                            System.out.println("¡MISION COMPLETADA!");
+                        } else {
+                            System.out.println("Todos los miembros del equipo han muerto! La mision fue un fracaso.");
+                        }
+                    } else {
+                        System.out.println("No hay tripulantes para la batalla final");
+                    }
                     break;
-
                 case 4:
-                    condicion = false;
-                    System.out.println("EJECUCION FINALIZADA, VUELVA PRONTO.....");
+                    System.out.println("Programa Finalizado");
                     break;
                 default:
-                    System.out.println("OPCION INGRESADA NO VALIDA, VUELVA A INTENTAR......");
+                    System.out.println("Opcion no valida");
+                    break;
+
             }
-        }
+        } while (opcion != 4);
     }
+
     public static void reclutarTripulante(ArrayList<Tripulante> tripulacion) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
         String nombre;
         String especie;
         String planeta;
         System.out.println("Ingrese el nombre del tripulante: ");
-        nombre = scanner.nextLine();
+        nombre = scanner2.nextLine();
         System.out.println("Ingrese la especie del tripulante: ");
-        especie = scanner.nextLine();
+        especie = scanner2.nextLine();
         System.out.println("Ingrese el planeta de origen del tripulante");
-        planeta = scanner.nextLine();
+        planeta = scanner2.nextLine();
 
         int habilidadAtaque = 0;
         int habilidadDefensa = 0;
@@ -80,17 +78,19 @@ public class Ex2P1_ExdiMatamoros {
         int estadoHabilidades = 1;
         while (estadoHabilidades != 0) {
             System.out.println("Ingrese la hablidad de ataque del tripulante(entre 100 y 300): ");
-            habilidadAtaque = scanner.nextInt();
+            habilidadAtaque = scanner2.nextInt();
             System.out.println("Ingrese la habilidad de defensa del tripulante(entre 100 y 300): ");
-            habilidadDefensa = scanner.nextInt();
+            habilidadDefensa = scanner2.nextInt();
             System.out.println("Ingrese la habilidad tecnica del tripulante (entre 100 y 300): ");
-            habilidadTecnica = scanner.nextInt();
+            habilidadTecnica = scanner2.nextInt();
             if (habilidadAtaque >= 100 && habilidadAtaque <= 300 && habilidadDefensa >= 100 && habilidadDefensa <= 300 && habilidadTecnica >= 100 && habilidadTecnica <= 300) {
                 estadoHabilidades = 0;
             } else {
                 System.out.println("Las habilidades deben estar entre 100 y 300, intente nuevamente. ");
             }
         }
+        Tripulante tripulante = new Tripulante(nombre, especie, planeta, habilidadAtaque, habilidadDefensa, habilidadTecnica);
+        tripulacion.add(tripulante);
     }
 
     public static void listarTripulantes(ArrayList<Tripulante> tripulacion) {
@@ -109,18 +109,18 @@ public class Ex2P1_ExdiMatamoros {
         }
     }
 
-    public static boolean ataqueCollector(ArrayList<Tripulante> tripulacion) {
-        Scanner scanner2 = new Scanner(System.in);
+    public static boolean batalla(ArrayList<Tripulante> tripulacion) {
+        Scanner scanner3 = new Scanner(System.in);
         Random random = new Random();
 
         int turnos = 0;
         int bajas = 0;
 
-        while (turnos < 5) {
+        while (turnos < 5 && !tripulacion.isEmpty()) {
             int situacionAleatoria = random.nextInt(8);
 
             System.out.println("-+-+-+- Situación Aleatoria -+-+-+-");
-            System.out.println("Descripcion: " + obtenerDescripcionSituacionAleatoria(situacionAleatoria));
+            System.out.println("Descripción: " + obtenerDescripcionSituacionAleatoria(situacionAleatoria));
             System.out.println("Miembros de la tripulacion:");
 
             for (int i = 0; i < tripulacion.size(); i++) {
@@ -128,73 +128,68 @@ public class Ex2P1_ExdiMatamoros {
             }
 
             System.out.print("Seleccione un miembro del equipo para enfrentar la situacion: ");
-            int indiceTripulante = scanner2.nextInt();
+            int indiceTripulante = scanner3.nextInt();
 
             if (indiceTripulante >= 0 && indiceTripulante < tripulacion.size()) {
                 Tripulante tripulanteElegido = tripulacion.get(indiceTripulante);
 
-                int resultadoHabilidad = random.nextInt(201) + 100;
+                int resultadoSituacion = random.nextInt(201) + 100;
 
-                System.out.println("Resultado de habilidad del tripulante: " + resultadoHabilidad);
+                System.out.println("Puntos requeridos para completar la situacion: " + resultadoSituacion);
 
-                if (habilidadSuficiente(tripulanteElegido, situacionAleatoria, resultadoHabilidad)) {
+                if (habilidadSuficiente(tripulanteElegido, situacionAleatoria, resultadoSituacion)) {
                     System.out.println(tripulanteElegido.getNombre() + " tuvo exito en su mision!!");
                 } else {
-                    System.out.println(tripulanteElegido.getNombre() + " murio en batalla. No logro terminar su misión. Si quedan miembros del equipo, intente de nuevo.");
+                    System.out.println(tripulanteElegido.getNombre() + " murio en batalla. No logro terminar su mision. Si quedan miembros del equipo, intente de nuevo.");
                     tripulacion.remove(tripulanteElegido);
                     bajas++;
                 }
             } else {
-                System.out.println("¡Indice de tripulante no valido!");
+                System.out.println("Posicion no valida");
             }
 
             turnos++;
         }
+        if (!tripulacion.isEmpty()) {
+            System.out.println("-+-+-+- Situacion Final -+-+-+-");
+            System.out.println("Descripcion: Llego al nucleo de la nave. Destruya el ultima arma de los collectors para triunfar (necesidad de ataque)");
+            System.out.println("Miembros de la tripulacion:");
+            for (int i = 0; i < tripulacion.size(); i++) {
+                System.out.println(i + "- " + tripulacion.get(i).toString());
+            }
 
-        // Situación adicional para el quinto turno
-        System.out.println("-+-+-+- Situación Final -+-+-+-");
-        System.out.println("Descripcion: Llego al nucleo de la nave. Destruya la ultima arma de los collectors para triunfar (necesidad de ataque)");
+            System.out.print("Seleccione un miembro del equipo: ");
+            int indiceTripulanteFinal = scanner3.nextInt();
 
-        System.out.println("Miembros de la tripulacion:");
+            if (indiceTripulanteFinal >= 0 && indiceTripulanteFinal < tripulacion.size()) {
+                Tripulante tripulanteElegidoFinal = tripulacion.get(indiceTripulanteFinal);
 
-        for (int i = 0; i < tripulacion.size(); i++) {
-            System.out.println(i + "- " + tripulacion.get(i).toString());
-        }
+                int resultadoHabilidadFinal = random.nextInt(201) + 100;
 
-        System.out.print("Seleccione un miembro del equipo para destruir la ultima arma: ");
-        int indiceTripulanteFinal = scanner2.nextInt();
+                System.out.println("Necesidad de puntos para destruir el arma: " + resultadoHabilidadFinal);
 
-        if (indiceTripulanteFinal >= 0 && indiceTripulanteFinal < tripulacion.size()) {
-            Tripulante tripulanteElegidoFinal = tripulacion.get(indiceTripulanteFinal);
-
-            int resultadoHabilidadFinal = random.nextInt(201) + 100;
-
-            System.out.println("Resultado de habilidad del tripulante para destruir la ultima arma: " + resultadoHabilidadFinal);
-
-            if (resultadoHabilidadFinal >= tripulanteElegidoFinal.getHabilidadAtaque()) {
-                System.out.println(tripulanteElegidoFinal.getNombre() + " tuvo exito en destruir la ultima arma! La misión fue un exito.");
-                return true;
+                if (resultadoHabilidadFinal <= tripulanteElegidoFinal.getHabilidadAtaque()) {
+                    System.out.println(tripulanteElegidoFinal.getNombre() + " tuvo exito en destruir el ultima arma! La mision fue un exito.");
+                    return true;
+                } else {
+                    System.out.println(tripulanteElegidoFinal.getNombre() + " no tuvo exito en destruir el ultimo arma. La mision fracaso!");
+                    tripulacion.remove(tripulanteElegidoFinal);
+                    bajas++;
+                }
             } else {
-                System.out.println(tripulanteElegidoFinal.getNombre() + " no tuvo exito en destruir la ultima arma. La misión fue un fracaso!");
-                tripulacion.remove(tripulanteElegidoFinal);
-                bajas++;
+                System.out.println("Indice de tripulante no valido para la situación final!");
             }
         } else {
-            System.out.println("¡Índice de tripulante no válido para la situación final!");
+            System.out.println();
         }
 
-        if (bajas == tripulacion.size()) {
-            System.out.println("¡Todos los miembros del equipo han muerto! La misión fue un fracaso.");
-            return false;
-        }
-
-        return true;
+        return false;
     }
 
     public static String obtenerDescripcionSituacionAleatoria(int situacionAleatoria) {
         switch (situacionAleatoria) {
             case 0:
-                return "Un escudo se interpone en el camino, se necesita desactivar (necesidad técnica)";
+                return "Un escudo se interpone en el camino, se necesita desactivar (necesidad tecnica)";
             case 1:
                 return "El sistema de seguridad se activo, se necesita desactivar (necesidad tecnica)";
             case 2:
@@ -210,20 +205,26 @@ public class Ex2P1_ExdiMatamoros {
             case 7:
                 return "Elija a alguien para liderar al equipo a través de un escuadrón de collectors (necesidad ofensiva)";
             default:
-                return "Descripcion de la situacion no especificada";
+                return "Descripción de la situacion no especificada";
         }
     }
 
-    private static boolean habilidadSuficiente(Tripulante tripulante, int situacionAleatoria, int resultadoHabilidad) {
+    private static boolean habilidadSuficiente(Tripulante tripulante, int situacionAleatoria, int resultadoSituacion) {
         String necesidad = obtenerTipoNecesidad(situacionAleatoria);
 
         switch (necesidad) {
             case "tecnica":
-                return resultadoHabilidad >= tripulante.getHabilidadTecnica();
+                System.out.println("Comparando habilidad técnica del tripulante (" + tripulante.getHabilidadTecnica()
+                        + ") con el resultado de la situación (" + resultadoSituacion + ")...");
+                return resultadoSituacion <= tripulante.getHabilidadTecnica();
             case "ataque":
-                return resultadoHabilidad >= tripulante.getHabilidadAtaque();
+                System.out.println("Comparando habilidad de ataque del tripulante (" + tripulante.getHabilidadAtaque()
+                        + ") con el resultado de la situación (" + resultadoSituacion + ")...");
+                return resultadoSituacion <= tripulante.getHabilidadAtaque();
             case "defensa":
-                return resultadoHabilidad >= tripulante.getHabilidadDefensa();
+                System.out.println("Comparando habilidad de defensa del tripulante (" + tripulante.getHabilidadDefensa()
+                        + ") con el resultado de la situación (" + resultadoSituacion + ")...");
+                return resultadoSituacion <= tripulante.getHabilidadDefensa();
             default:
                 System.out.println("Necesidad no reconocida.");
                 return false;
@@ -244,8 +245,9 @@ public class Ex2P1_ExdiMatamoros {
             case 5:
                 return "defensa";
             default:
-                System.out.println("Situacin no reconocida.");
+                System.out.println("Situación no reconocida.");
                 return "";
         }
     }
 }
+
